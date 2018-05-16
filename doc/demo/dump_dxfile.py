1d2340d04c5db3de3452213cb786cf2943918d12
Francesco De Carlo <decarlof@gmail.com>
2018-05-16 15:14:21 -0500

    parsing file name

--- doc/demo/dump_dxfile.py
+++ doc/demo/dump_dxfile.py
@@ -10,7 +10,10 @@
 
 from __future__ import print_function
 
+import os
 import h5py
+import sys
+import argparse
 import dxchange.reader as dxreader
 
 
@@ -18,11 +21,12 @@ def print_hdf5_file_structure(file_name) :
     """Prints the HDF5 file structure"""
     filee = h5py.File(file_name, 'r') # open read-only
     item = filee #["/Configure:0000/Run:0000"]
-    print_hdf5_item_structure(item)
+    print_hdf5_item_structure(item, file_name)
     filee.close()
  
-def print_hdf5_item_structure(g, offset='    ') :
+def print_hdf5_item_structure(g, file_name, offset='    ') :
     """Prints the input file/group/dataset (g) name and begin iterations on its content"""
+
     if   isinstance(g, h5py.File) :
         print (g.file, 'File:', g.name)
  
@@ -37,7 +41,7 @@ def print_hdf5_item_structure(g, offset='    ') :
         elif g.name == '/exchange/data_dark' :
             print('data dark')
         else:
-            print (g.name, '=', dxreader.read_hdf5(fname,  g.name))
+            print (g.name, '=', dxreader.read_hdf5(file_name,  g.name))
  
     elif isinstance(g, h5py.Group) :
         print ('Group:', g.name)
@@ -50,11 +54,20 @@ def print_hdf5_item_structure(g, offset='    ') :
         for key,val in dict(g).iteritems() :
             subg = val
             #print (offset, key )#,"   ", subg.name #, val, subg.len(), type(subg),
-            print_hdf5_item_structure(subg, offset + '    ')
+            print_hdf5_item_structure(subg, file_name, offset + '    ')
  
-if __name__ == "__main__" :
-        
-    # Set path to the micro-CT data.
-    fname = '/tomobank/tomo_00001/tomo_00001.h5'
-    print_hdf5_file_structure(fname)
 
+def main(arg):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fname", help="DataExchange file name: /data/sample.h5")
+    args = parser.parse_args()
+
+    # Set path to the micro-CT data to reconstruct.
+    fname = args.fname
+    if os.path.isfile(fname): 
+        print(fname)   
+        print_hdf5_file_structure(fname)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
